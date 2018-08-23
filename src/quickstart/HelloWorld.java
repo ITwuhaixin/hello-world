b4f19c63b1caa35c05a9e93b1ccc82102d4a97a0
wuhaixin <itwuhaixin@gmail.com>
2018-08-23 13:53:08 +0800

    Idea上传项目到github

---
+++ src/quickstart/HelloWorld.java
@@ -0,0 +1,16 @@
+package quickstart;
+
+/**
+ * create by wuhaixin 2018-8-23
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("one two three!");
+        String name = "John smith";
+        System.out.println("one two three!");
+        System.out.println(name);
+
+    }
+
+}
